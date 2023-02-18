8733bf75534427bcbec4b3cf9a32b0222a0b9118
Mohammed Adel <mohammedadel111111@gmail.com>
2023-02-18 18:16:03 +0200

    create node card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/style/app_style.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:note_app/widgets/note_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -51,8 +52,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 }
                 if (snapShot.hasData) {
                   return GridView(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2));
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    children: snapShot.data!.docs
+                        .map((note) => noteCard(() {}, note)),
+                  );
                 }
                 return Text(
                   "There's No Notes",
